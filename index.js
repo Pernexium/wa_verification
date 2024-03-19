77a1bb54e45f2f392ec03f25871aa4a9401f5260
Carlos Espejel <155459994+CEspejelPernexium@users.noreply.github.com>
2024-03-19 07:21:59 -0600

    Change save creds

--- index.js
+++ index.js
@@ -1,67 +1,79 @@
 const express = require('express');
-const { DisconnectReason, useMultiFileAuthState } = require("@whiskeysockets/baileys");
+const {
+    DisconnectReason,
+    useMultiFileAuthState,
+} = require("@whiskeysockets/baileys");
 const makeWASocket = require("@whiskeysockets/baileys").default;
+const { useMySQLAuthState } = require("mysql-baileys");
+const QRCode = require("qrcode");
 
-async function startServerAfterConnection() {
-    const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
-    const sock = makeWASocket({
+
+const socks = {};
+
+async function connectToWA(sessionName) {
+    const { state, saveCreds, removeCreds } = await useMySQLAuthState({
+        session: sessionName,
+        host: "pernexium-db.cfioetbrvik6.us-east-2.rds.amazonaws.com",
+        port: 3306,
+        user: "analitica_espejel",
+        password: "7s4xV8'YpK4}&I<",
+        database: "analitica",
+    });
+
+    socks[sessionName] = makeWASocket({
         printQRInTerminal: true,
         auth: state,
     });
 
-    sock.ev.on("connection.update", async (update) => {
-        const { connection, lastDisconnect } = update || {};
+    socks[sessionName].ev.on("connection.update", async (update) => {
+        const { connection, lastDisconnect, qr } = update || {};
+        if (qr) {
+            console.log(qr);
+            QRCode.toFile(`./QRCodes/${sessionName}.png`, qr);
+        }
 
-        if (connection === "open") {
-            const app = express();
-
-            app.use(express.json());
-
-            app.post('/validate', async (req, res) => {
-                const phoneNumber = req.body.phoneNumber;
-
-                const [result] = await sock.onWhatsApp(phoneNumber);
-
-                if (result === undefined) {
-                    res.send({
-                        exists: false,
-                        message: `El número de teléfono ${phoneNumber} no existe en WhatsApp`
-                    });
-                } else if (result.exists) {
-                    res.send({
-                        exists: true,
-                        message: `El número de teléfono ${phoneNumber} existe en WhatsApp`
-                    });
-                } else {
-                    res.send({
-                        exists: false,
-                        message: `El número de teléfono ${phoneNumber} no existe en WhatsApp`
-                    });
-                }
-            });
-
-            app.listen(80, () => {
-                console.log(`Servidor escuchando en el puerto ${80}`);
-            });
-        } else if (connection === "close") {
+        if (connection === "close") {
             const shouldReconnect =
                 lastDisconnect?.error?.output?.statusCode !==
                 DisconnectReason.loggedOut;
 
             if (shouldReconnect) {
-                startServerAfterConnection();
+                connectToWA(sessionName);
             }
         }
     });
 
-    sock.ev.on("messages.update", (messageInfo) => {
-        console.log(messageInfo);
-    });
-
-    sock.ev.on("messages.upsert", (messageInfoUpsert) => {
-        console.log(messageInfoUpsert);
-    });
-    sock.ev.on("creds.update", saveCreds);
+    socks[sessionName].ev.on("creds.update", saveCreds);
 }
 
-startServerAfterConnection();
+connectToWA("session5");
+
+const app = express();
+app.use(express.json());
+
+app.post('/validate', async (req, res) => {
+    const phoneNumber = req.body.phoneNumber;
+
+    const [result] = await socks["session5"].onWhatsApp(phoneNumber);
+
+    if (result === undefined) {
+        res.send({
+            exists: false,
+            message: `El número de teléfono ${phoneNumber} no existe en WhatsApp`
+        });
+    } else if (result.exists) {
+        res.send({
+            exists: true,
+            message: `El número de teléfono ${phoneNumber} existe en WhatsApp`
+        });
+    } else {
+        res.send({
+            exists: false,
+            message: `El número de teléfono ${phoneNumber} no existe en WhatsApp`
+        });
+    }
+});
+
+app.listen(80, () => {
+    console.log(`Servidor escuchando en el puerto ${80}`);
+});
